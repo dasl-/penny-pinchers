b7d17878511435a44092f5dbaa2edbba207898b1
davidleibovic <david.leibovic@gmail.com>
2015-10-03 08:54:11 -0400

    bold

--- template/homepage/recent_activity_row.php
+++ template/homepage/recent_activity_row.php
@@ -2,7 +2,7 @@
 	<td><?= timeAgo($recent_activity["date"]) ?></td>
 	<td>
 		<a href="/charges/<?= $recent_activity['charge_id']?>">
-			<?= $recent_activity["user_name"] ?> added a new charge for:
+			<b><?= $recent_activity["user_name"] ?></b> added a new charge for:
 			<b><?= $recent_activity["description"] ?></b>
 			(<?= formatMoney($recent_activity["amount"]) ?>).
 		</a>
